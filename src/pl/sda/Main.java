1127c9ef18601c20709969b78d0f0d4c8552f6ec
Jakub <kubplon@gmail.com>
2020-05-23 10:08:51 +0200

    My first commit

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,11 @@
+package pl.sda;
+
+public class Main {
+
+    //1. VCS -> Import into version control -> Create Git repository..
+    //2. View -> Tool windows -> Git
+    //3. Zaznacz plik -> Commit File (ewentualnie Git -> Commit File)
+    //4. Zaznacz pliki -> Commit Files (ewentualnie Git -> Commit Files)
+    public static void main(String[] args) {
+    }
+}
